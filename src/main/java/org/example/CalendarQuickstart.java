b2edde1bfd02c415287c1f816b56eab100bfe5e0
Ava Allwardt <ava.allwardt@saintandrews.net>
2022-11-11 12:54:03 -0500

    Assignment notes added.

--- src/main/java/org/example/CalendarQuickstart.java
+++ src/main/java/org/example/CalendarQuickstart.java
@@ -1,2 +1,154 @@
-package org.example;public class CalendarQuickstart {
+package org.example;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.Events;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+/* class to demonstarte use of Calendar events list API */
+public class CalendarQuickstart {
+    /**
+     * Application name.
+     */
+    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    /**
+     * Directory to store authorization tokens for this application.
+     */
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES =
+            // don't want it to be CALENDAR_READONLY -- want just CALENDAR so that we can modify it
+            Collections.singletonList(CalendarScopes.CALENDAR);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    // this gives us authorization to do stuff with the user
+    // this method is in every single Google API
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+            throws IOException {
+        // Load client secrets.
+        // file path is unique to application
+        InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        //returns an authorized Credential object.
+        return credential;
+    }
+
+
+    // summary is used to set title of an event
+    // when it is a writable method, it means you can override it
+
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Calendar service =
+                new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                        .setApplicationName(APPLICATION_NAME)
+                        .build();
+
+        // getCredentials calls the method above it to validate the service object
+        // the service object is the hook into the google service to actually do something
+
+        // primary is how Google refers to the main calendar of the
+        service.events().insert("primary", makeEvent("Help Ava with CSV", "earth", "Fun with CSV")).execute();
+        System.out.println("Done building event.");
+
+
+/*
+        // List the next 10 events from the primary calendar.
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events = service.events().list("primary")
+                .setMaxResults(10)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+        List<Event> items = events.getItems();
+        if (items.isEmpty()) {
+            System.out.println("No upcoming events found.");
+        } else {
+            System.out.println("Upcoming events");
+            // we have an event class imported using gradle
+            // usually you follow API documentation specifically
+            // go to google calendar api on internet to find all the classes it makes available to you
+            for (Event event : items) {
+                DateTime start = event.getStart().getDateTime();
+                if (start == null) {
+                    start = event.getStart().getDate();
+                }
+                System.out.printf("%s (%s)\n", event.getSummary(), start);
+            }
+        }
+
+ */
+    }
+
+    public static Event makeEvent(String summary, String eventLocation, String eventDescription){
+        Event event = new Event()
+                .setSummary(summary)
+                .setLocation(eventLocation)
+                .setDescription(eventLocation);
+        // can do all of the setters on one line or can also do line breaks (as shown above)
+        // it makes startDateTime a separate object type and then gives the event an instance variable of that type (compartmentalizes it)
+        // you have to import classes because they are in a package location and the uimporta statement tells the code where to find it
+        // use military time
+        DateTime startDateTime = new DateTime("2022-11-03T09:00:00-07:00");
+        EventDateTime start = new EventDateTime()
+                .setDateTime(startDateTime)
+                // can find the list of time zones from tz, google uses it in their code and since we are using the google library, we use the same tz stuff
+                .setTimeZone("America/New_York");
+        event.setStart(start);
+        return event;
+    }
+
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        // token = has access to do stuff on the user you want
+        // he will try setting the end date before the start date -- can either make teh start date teh end date and vide versa or just reverse it
+        // the user does not need to insert their email, the app is only limited to your personal calendar
+        // not graded on app design, only needs to be functional
+        // biggest thing is the date
+        // can use a date picker in scene builder (java has a date class -- can google for this)
     }
 }
